5b5706f5827b016d0a1dd5a3eb5908e5e7cb1b88
SonnAdolf <programeryu@163.com>
2016-10-06 09:39:08 +0800

    some little change

--- lsp-chat-client.c
+++ lsp-chat-client.c
@@ -11,17 +11,38 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <errno.h>
 
 int main(int argc, char **argv)
 {
 	int sockfd, len;
 	struct sockaddr_in dest;
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	//1.create socket object
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("socket");
+		exit(errno);
+	}
 	printf("socket created.\n");
+	//set dest 0
+	bzero(&dest, sizeof(dest));
+	//address protocal
 	dest.sin_family = AF_INET;
+	//the port of server
 	dest.sin_port = htons(atoi(argv[2]));
-	inet_aton(argv[1], (struct in_addr *) &dest.sin_addr.s_addr);
-	connect(sockfd, (struct sockaddr *) &dest, sizeof(dest));
+	//get the ip of server
+	if (inet_aton(argv[1], (struct in_addr *) &dest.sin_addr.s_addr) == 0)
+	{
+		perror(argv[1]);
+		exit(errno);
+	}
+	//connect the server
+	if(connect(sockfd, (struct sockaddr *) &dest, sizeof(dest)) == -1)
+	{
+		perror("connect");
+		exit(errno);
+	}
 	printf("server connected\n");
 	return 0;
 }
--- lsp-chat-sever.c
+++ lsp-chat-sever.c
@@ -1,6 +1,6 @@
 /**
  * server program of lsp-chat 
- * by 	WHOAMI localhost purstar
+ * by 	WHOAMI localhost purstar Atum
  * Oct 5 2016
  * version 1.0	
  **/
@@ -11,34 +11,57 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
+
 int main(int argc, char *argv[])
 {
-	unsigned int myport, lisnum;
+	unsigned int my_port, lisnum;
 	int sockfd, new_fd;
 	socklen_t len;
 	if (argv[2])
-		myport = atoi(argv[2]);
+		my_port = atoi(argv[2]);
 	else
-		myport = 7575;
+		my_port = 7575;
 	if (argv[3])
 		lisnum = atoi(argv[3]);
 	else
 		lisnum = 5; 
 	struct sockaddr_in server_addr, clients_addr;
-	// create socket object
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	//create socket object
+	if(sockfd = socket(AF_INET, SOCK_STREAM, 0)==-1)
+	{
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+	//set server_addr 0
+	bzero(&server_addr, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(myport);
 	if (argv[1])
 		server_addr.sin_addr.s_addr = inet_addr(argv[1]);
 	else
 		server_addr.sin_addr.s_addr = INADDR_ANY;
-	bind(sockfd, (struct sockaddr *) &server_addr, sizeof(struct sockaddr));
-	listen(sockfd,lisnum);
+	//bind the socket with ip and port
+	if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(struct sockaddr)) == -1)
+	{
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+	//listern and wait for connecting
+	if(listen(sockfd,lisnum) == -1)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
 	printf("wait for connect\n");
 	len = sizeof(struct sockaddr);
-	new_fd = accept(sockfd, (struct sockaddr *) &clients_addr, &len);
-	printf("server: got connection from %s, port %d, socket %d\n",inet_ntoa(clients_addr.sin_addr),ntohs(clients_addr.sin_port),new_fd);
+	//get the client connection
+	if(new_fd = accept(sockfd, (struct sockaddr *) &clients_addr, &len) == -1)
+	{
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+	else
+		printf("server: got connection from %s, port %d, socket %d\n",inet_ntoa(clients_addr.sin_addr),ntohs(clients_addr.sin_port),new_fd);
 	return 0;
 }
 
